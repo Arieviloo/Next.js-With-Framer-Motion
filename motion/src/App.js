a0e5b2ffee9fd94c11513ce66376b8b7d43095b2
Jadiê <jadieoliveira00@gmail.com>
2020-06-04 23:15:49 -0300

    init

---
+++ motion/src/App.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { useState } from "react";
+import { Example } from "./Example";
+import { Refresh } from "./Refresh";
+
+import "./styles.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Refresh onClick={() => setCount(count + 1)} />
+      <div className="example-container">
+        <Example key={count} />
+      </div>
+    </>
+  );
+};
+
+export default App;
